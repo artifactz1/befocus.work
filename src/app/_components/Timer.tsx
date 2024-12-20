96ed45cae93c8f3000d09431ea76dc6d9a4fb7e7
artifactz1 <punsalang.art@gmail.com>
2024-12-20 03:50:33 -0800

    Menubar: added menubar to have settigs button in it

--- src/app/_components/Timer.tsx
+++ src/app/_components/Timer.tsx
@@ -1,23 +1,70 @@
 "use client";
 
+import Link from "next/link";
+import * as React from "react";
+
+import { cn } from "../../lib/utils";
+// import { Icons } from "@/compon../../lib/utilsents/icons"
+
 import { Pause, Play, RotateCcw, SkipForward } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
+import { Settings } from "../_components/Settings";
+import { DarkModeToggle } from "./DarkModeToggle";
+
 import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "~/components/ui/navigation-menu";
-import { Settings } from "../_components/Settings";
-import { DarkModeToggle } from "./DarkModeToggle";
 
 const formatTime = (seconds: number): string => {
   const mins = Math.floor(seconds / 60);
   const secs = seconds % 60;
   return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
 };
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Hover Card",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Progress",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Scroll-area",
+    href: "/docs/primitives/scroll-area",
+    description: "Visually or semantically separates content.",
+  },
+  {
+    title: "Tabs",
+    href: "/docs/primitives/tabs",
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+  },
+  {
+    title: "Tooltip",
+    href: "/docs/primitives/tooltip",
+    description:
+      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+  },
+];
 
 export default function Timer() {
   const [sessions, setSessions] = useState(6);
@@ -108,7 +155,7 @@ export default function Timer() {
 
   return (
     <div className="container mx-auto flex h-screen w-screen flex-col items-center justify-center">
-      <div className="relative flex w-full items-center justify-between">
+      <div className="flex w-full items-center justify-between">
         {/* Focus/Break */}
         <div className="font-regular text-9xl">
           {isWorking ? "Focus" : "Break"}
@@ -156,28 +203,124 @@ export default function Timer() {
         >
           <SkipForward className="h-6 w-6" />
         </Button>
+
         <DarkModeToggle />
       </div>
 
-      <NavigationMenu className="absolute z-0">
+      {/* <div className="flex flex-col justify-between">
+        <div>
+          <h2 className="mb-4 text-2xl font-medium">Settings</h2>
+          <Settings
+            sessions={sessions}
+            workDuration={workDuration / 60}
+            breakDuration={breakDuration / 60}
+            onSettingsChange={handleSettingsChange}
+          />
+        </div>
+      </div> */}
+      <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
+            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li className="row-span-3">
+                  <NavigationMenuLink asChild>
+                    <Link
+                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                      href="/"
+                    >
+                      {/* <Icons.logo className="h-6 w-6" /> */}
+                      <div className="mb-2 mt-4 text-lg font-medium">
+                        shadcn/ui
+                      </div>
+                      <p className="text-sm leading-tight text-muted-foreground">
+                        Beautifully designed components built with Radix UI and
+                        Tailwind CSS.
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <ListItem href="/docs" title="Introduction">
+                  Re-usable components built using Radix UI and Tailwind CSS.
+                </ListItem>
+                <ListItem href="/docs/installation" title="Installation">
+                  How to install dependencies and structure your app.
+                </ListItem>
+                <ListItem href="/docs/primitives/typography" title="Typography">
+                  Styles for headings, paragraphs, lists...etc
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
             <NavigationMenuTrigger>Settings</NavigationMenuTrigger>
-            <NavigationMenuContent className="h-[200px] w-[100vw] p-20">
-              {/* <NavigationMenuLink>Link</NavigationMenuLink> */}
-              <div className="flex flex-col justify-between">
-                <h2 className="text-2xl font-medium">Settings</h2>
-                <Settings
-                  sessions={sessions}
-                  workDuration={workDuration / 60}
-                  breakDuration={breakDuration / 60}
-                  onSettingsChange={handleSettingsChange}
-                />
+            <NavigationMenuContent>
+              <div className="grid w-[400px] gap-3 p-4 md:w-[500px] lg:w-[600px]">
+                <div className="rounded-md bg-card p-6 shadow-md">
+                  <h2 className="mb-4 text-2xl font-medium">Settings</h2>
+                  <Settings
+                    sessions={sessions}
+                    workDuration={workDuration / 60}
+                    breakDuration={breakDuration / 60}
+                    onSettingsChange={handleSettingsChange}
+                  />
+                </div>
               </div>
             </NavigationMenuContent>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Documentation
+              </NavigationMenuLink>
+            </Link>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
     </div>
   );
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
