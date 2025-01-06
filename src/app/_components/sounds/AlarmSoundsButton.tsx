b875d23d0da5661c1befe8b97da7be3196069326
artifactz1 <punsalang.art@gmail.com>
2025-01-06 04:05:03 -0800

    Code clean up: deleted comments

--- src/app/_components/sounds/AlarmSoundsButton.tsx
+++ src/app/_components/sounds/AlarmSoundsButton.tsx
@@ -1,63 +1,6 @@
-// "use client";
-
-// import {
-//   Select,
-//   SelectContent,
-//   SelectGroup,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "~/components/ui/select";
-// import { Separator } from "~/components/ui/separator";
-// import { useSoundsStore } from "~/store/useSoundsStore";
-
-// export default function AlarmSoundsButton() {
-//   const { sounds, alarmId, setAlarmId } = useSoundsStore();
-
-//   // Provide a default value for alarmId if it's undefined or null
-//   const currentAlarm = alarmId || "";
-
-//   console.log("alarmId:", alarmId);
-//   console.log("sounds:", sounds);
-
-//   return (
-//     <div>
-//       <Separator className="my-4 bg-white" />
-//       <h3 className="text-center">Alarm Sound</h3>
-//       <div className="flex-row space-y-2">
-//         <Select
-//           value={currentAlarm} // Ensure value is a string
-//           onValueChange={(value) => {
-//             if (value) {
-//               setAlarmId(value); // Safely update alarmId
-//             } else {
-//               console.warn("Invalid value selected:", value);
-//             }
-//           }}
-//         >
-//           <SelectTrigger className="w-1/3">
-//             <SelectValue placeholder="Select an alarm" />
-//           </SelectTrigger>
-//           <SelectContent>
-//             <SelectGroup>
-//               {Object.keys(sounds)
-//                 .filter((soundId) => !sounds[soundId]?.isCustom) // Only non-custom sounds
-//                 .map((soundId) => (
-//                   <SelectItem key={soundId} value={soundId}>
-//                     {soundId}
-//                   </SelectItem>
-//                 ))}
-//             </SelectGroup>
-//           </SelectContent>
-//         </Select>
-//       </div>
-//     </div>
-//   );
-// }
-
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Select,
   SelectContent,
@@ -125,3 +68,4 @@ export default function AlarmSoundsButton() {
     </div>
   );
 }
+
