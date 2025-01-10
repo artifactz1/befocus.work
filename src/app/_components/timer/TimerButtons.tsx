94a0d47241c53ef101168966726545c575cecf09
artifactz1 <punsalang.art@gmail.com>
2025-01-10 03:22:27 -0800

    Tooltip: add shadcnui component ot timer buttons

--- src/app/_components/timer/TimerButtons.tsx
+++ src/app/_components/timer/TimerButtons.tsx
@@ -2,6 +2,12 @@
 
 import { Pause, Play, RotateCcw, SkipForward } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
 import { useTimerStore } from "~/store/useTimerStore";
 import { DarkModeToggle } from "../DarkModeToggle";
 
@@ -10,29 +16,58 @@ export default function TimerButtons() {
 
   return (
     <div className="flex items-center justify-center space-x-1">
-      <Button
-        onClick={toggleTimer}
-        variant="outline"
-        size="lg"
-        className="h-12 w-32"
-      >
-        {isRunning ? (
-          <Pause className="h-6 w-6" strokeWidth={3} />
-        ) : (
-          <Play className="h-6 w-6" strokeWidth={3} />
-        )}
-      </Button>
-      <Button onClick={reset} variant="outline" size="lg" className="h-12 w-32">
-        <RotateCcw className="h-6 w-6" strokeWidth={3} />
-      </Button>
-      <Button
-        onClick={skipToNextSession}
-        variant="outline"
-        size="lg"
-        className="h-12 w-32"
-      >
-        <SkipForward className="h-6 w-6" strokeWidth={3} />
-      </Button>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              onClick={toggleTimer}
+              variant="outline"
+              size="lg"
+              className="h-12 w-32"
+            >
+              {isRunning ? (
+                <Pause className="h-6 w-6" strokeWidth={3} />
+              ) : (
+                <Play className="h-6 w-6" strokeWidth={3} />
+              )}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>{isRunning ? "Pause" : "Play"}</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              onClick={reset}
+              variant="outline"
+              size="lg"
+              className="h-12 w-32"
+            >
+              <RotateCcw className="h-6 w-6" strokeWidth={3} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Reset Time</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              onClick={skipToNextSession}
+              variant="outline"
+              size="lg"
+              className="h-12 w-32"
+            >
+              <SkipForward className="h-6 w-6" strokeWidth={3} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>Skip To Next</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <DarkModeToggle />
     </div>
   );
