a4f5334cd09a2e3168716a049c05636ca8a36858
artifactz1 <punsalang.art@gmail.com>
2025-02-01 08:55:34 -0800

    Added counter animation

--- src/app/_components/input/WorkDurationInput.tsx
+++ src/app/_components/input/WorkDurationInput.tsx
@@ -1,5 +1,6 @@
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
+import Counter from './Counter';
 
 interface BreakDurationInputProps {
   value: number;
@@ -19,8 +20,9 @@ export const WorkDurationInput: React.FC<BreakDurationInputProps> = ({
       type="number"
       min="1"
       value={value}
-      onChange={(e) => onChange(parseInt(e.target.value, 10) * 60 || 0)}
+      onChange={(e) => onChange(parseInt(e.target.value, 10) * 60 || 1)}
       className="border-gray-300 focus:border-black focus:ring-0"
     />
+    <Counter value={value}/>
   </div>
 );
