50539ffc4a811219a625daef8c6e8485bdfb42ed
artifactz1 <punsalang.art@gmail.com>
2025-02-02 08:37:17 -0800

    SessionsUI: fixed bug of changing user settings only after user presses save

--- src/app/_components/settings/SessionSettings.tsx
+++ src/app/_components/settings/SessionSettings.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "~/components/ui/button";
 import { useTimerStore } from "~/store/useTimerStore";
@@ -11,24 +12,28 @@ export const SessionSettings: React.FC = () => {
   const { sessions, workDuration, breakDuration, reset, updateSettings } =
     useTimerStore();
 
+  const [workTime, setWorkTime] = useState(workDuration);
+  const [breakTime, setBreakTime] = useState(breakDuration);
+  const [session, setSession] = useState(sessions);
+
   return (
     <div className="flex flex-col space-y-4">
       <WorkDurationInput
-        value={workDuration / 60}
-        onChange={(value) => updateSettings("workDuration", value)}
+        value={workTime / 60}
+        onChange={(value) => setWorkTime(value)}
       />
       <BreakDurationInput
-        value={breakDuration / 60}
-        onChange={(value) => updateSettings("breakDuration", value)}
+        value={breakTime / 60}
+        onChange={(value) => setBreakTime(value)}
       />
-      <SessionsInput
-        value={sessions}
-        onChange={(value) => updateSettings("sessions", value)}
-      />
+      <SessionsInput value={session} onChange={(value) => setSession(value)} />
       <div className="flex w-full justify-end">
         <Button
           className="rounded px-4 py-2"
           onClick={() => {
+            updateSettings("workDuration", workTime);
+            updateSettings("breakDuration", breakTime);
+            updateSettings("sessions", session);
             reset(); // Call reset
             toast("Session Settings has been saved", {
               description: "Sunday, December 03, 2023 at 9:00 AM",
@@ -39,7 +44,6 @@ export const SessionSettings: React.FC = () => {
               // },
             });
           }}
-          
         >
           Save
         </Button>
