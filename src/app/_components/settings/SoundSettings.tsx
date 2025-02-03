f5c6ffdd25a7780176f917bcfab20123b0f2ae3f
artifactz1 <punsalang.art@gmail.com>
2025-02-02 16:17:04 -0800

    SoundsUI: separated sounds into own components

--- src/app/_components/settings/SoundSettings.tsx
+++ src/app/_components/settings/SoundSettings.tsx
@@ -1,44 +1,21 @@
 "use client";
 
 import { Volume2 } from "lucide-react";
-import { Separator } from "~/components/ui/separator";
-import { useSoundsStore } from "~/store/useSoundsStore";
 import AddSoundButton from "../sounds/AddSoundButton";
 import AlarmSoundsButton from "../sounds/AlarmSoundsButton";
-import SoundButton from "../sounds/SoundButton";
+import AmbientSoundsButton from "../sounds/AmbientSoundsButton";
+import BgMusicSoundsButton from "../sounds/BgMusicSoundsButton";
 import ToggleAddMode from "../sounds/ToggleAddMode";
 import ToggleDeleteModeButton from "../sounds/ToggleDeleteMode";
-// import AmbientSoundsButton from '../sounds/AmbientSoundsButton';
 
 export default function SoundSettings() {
-  const { sounds } = useSoundsStore();
-
   return (
     <div className="flex h-full w-full select-none flex-col justify-end rounded-md no-underline outline-none">
       <Volume2 />
       <div className="mb-2 mt-4 text-lg font-bold">befocus/sounds</div>
       <AlarmSoundsButton />
-      {/* <AmbientSoundsButton /> */}
-
-      <Separator className="my-4 bg-white" />
-      <h3 className="text-left font-semibold"> Ambient Sounds</h3>
-      <div className="space-y-4">
-        {Object.keys(sounds)
-          .filter((soundId) => sounds[soundId]?.soundType === "ambient")
-          .map((soundId) => (
-            <SoundButton key={soundId} soundId={soundId} />
-          ))}
-      </div>
-
-      <Separator className="my-4 bg-white" />
-      <h3 className="text-center font-semibold"> Music Sounds</h3>
-      <div className="space-y-4">
-        {Object.keys(sounds)
-          .filter((soundId) => sounds[soundId]?.soundType === "bgMusic")
-          .map((soundId) => (
-            <SoundButton key={soundId} soundId={soundId} />
-          ))}
-      </div>
+      <AmbientSoundsButton />
+      <BgMusicSoundsButton />
 
       <AddSoundButton />
       <div className="mt-4 flex w-full items-center justify-center space-x-5 px-4 py-2">
--- src/app/_components/sounds/BgMusicSoundsButton.tsx
+++ src/app/_components/sounds/BgMusicSoundsButton.tsx
@@ -4,16 +4,16 @@ import { Separator } from "~/components/ui/separator";
 import { useSoundsStore } from "~/store/useSoundsStore";
 import SoundButton from "./SoundButton";
 
-export default function AmbientSoundsButton() {
+export default function BgMusicSoundsButton() {
   const { sounds } = useSoundsStore();
 
   return (
     <main>
       <Separator className="my-4 bg-white" />
-      <h3 className="text-left font-semibold mb-3"> Ambient Sounds</h3>
+      <h3 className="text-center font-semibold mb-3"> Music Sounds</h3>
       <div className="space-y-4">
         {Object.keys(sounds)
-          .filter((soundId) => sounds[soundId]?.soundType === "ambient")
+          .filter((soundId) => sounds[soundId]?.soundType === "bgMusic")
           .map((soundId) => (
             <SoundButton key={soundId} soundId={soundId} />
           ))}
