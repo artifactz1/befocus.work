0f0a627c3f4ea2261d165d5a75fa8ca22d866899
artifactz1 <punsalang.art@gmail.com>
2025-02-11 12:08:48 -0800

    completed adding session settings for mobile

--- src/app/_components/settings/MenuSettingsMobile.tsx
+++ src/app/_components/settings/MenuSettingsMobile.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import ToDoListMobile from "../to-do-list/ToDoListMobile";
-import SoundSettingsMobile from './SoundSettingsMobile';
+import SessionSettingsMobile from "./SessionSettingsMobile";
+import SoundSettingsMobile from "./SoundSettingsMobile";
 
 export default function MenuSettingsMobile() {
   return (
     <main className="block sm:hidden">
       <ToDoListMobile />
-      <SoundSettingsMobile/>
+      <SoundSettingsMobile />
+      <SessionSettingsMobile />
     </main>
   );
 }
---
+++ src/app/_components/settings/SessionSettingsMobile.tsx
@@ -0,0 +1,115 @@
+import { Timer } from "lucide-react";
+
+import { useState } from "react";
+import { toast } from "sonner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog";
+import { Button } from "~/components/ui/button";
+import { useTimerStore } from "~/store/useTimerStore";
+import { BreakDurationInput } from "../input/BreakDurationInput";
+import { SessionsInput } from "../input/SessionsInput";
+import { WorkDurationInput } from "../input/WorkDurationInput";
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "~/components/ui/drawer";
+import { Separator } from "~/components/ui/separator";
+
+export default function SessionSettingsMobile() {
+  const { sessions, workDuration, breakDuration, reset, updateSettings } =
+    useTimerStore();
+
+  const [workTime, setWorkTime] = useState(workDuration);
+  const [breakTime, setBreakTime] = useState(breakDuration);
+  const [session, setSession] = useState(sessions);
+
+  return (
+    <Drawer>
+      <DrawerTrigger>
+        <Timer />
+      </DrawerTrigger>
+      <DrawerContent className="max-h-screen px-2">
+        <DrawerHeader>
+          <DrawerTitle className="mx-1">
+            <Timer />
+            <div className="mb-2 mt-4 text-left text-lg font-bold">
+              befocus/sessions-settings
+            </div>
+            <Separator className="mt-4 bg-white" />
+          </DrawerTitle>
+          {/* <DrawerDescription>This action cannot be undone.</DrawerDescription> */}
+        </DrawerHeader>
+        <div className="overflow-y-auto px-5">
+          <div className="flex flex-col space-y-6">
+            <WorkDurationInput
+              value={workTime / 60}
+              onChange={(value) => setWorkTime(value)}
+            />
+            <BreakDurationInput
+              value={breakTime / 60}
+              onChange={(value) => setBreakTime(value)}
+            />
+            <SessionsInput
+              value={session}
+              onChange={(value) => setSession(value)}
+            />
+          </div>
+        </div>
+        <DrawerFooter>
+          <div className="flex w-full justify-end">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button>Save</Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your current session! Make sure to change your sessions
+                    settings before starting.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction
+                    onClick={() => {
+                      updateSettings("workDuration", workTime);
+                      updateSettings("breakDuration", breakTime);
+                      updateSettings("sessions", session);
+                      reset(); // Call reset
+                      toast("Session Settings has been saved", {
+                        description: "Sunday, December 03, 2023 at 9:00 AM",
+                        // Uncomment if you want the "Undo" action
+                        // action: {
+                        //   label: "Undo",
+                        //   onClick: () => reset(),
+                        // },
+                      });
+                    }}
+                  >
+                    Continue
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
