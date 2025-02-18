42f511e1840b097f7997f8b7277f6b23825854a3
artifactz1 <punsalang.art@gmail.com>
2025-02-17 20:58:08 -0800

    update(sound-player): matches main

--- apps/next/src/_components/GlobalSoundsPlayer.tsx
+++ apps/next/src/_components/GlobalSoundsPlayer.tsx
@@ -1,7 +1,7 @@
 "use client";
 // app/components/GlobalPlayer.tsx
 import ReactPlayer from "react-player";
-import { useSoundsStore } from "../store/useSoundsStore";
+import { useSoundsStore } from "~/store/useSoundsStore";
 
 const GlobalPlayer = () => {
   const { sounds } = useSoundsStore();
@@ -24,6 +24,7 @@ const GlobalPlayer = () => {
               playing={sound.playing}
               volume={sound.volume}
               controls={false} // Don't show controls as it's controlled globally
+              muted={!sound.playing}
               width="0"
               height="0"
             />
@@ -34,53 +35,3 @@ const GlobalPlayer = () => {
 };
 
 export default GlobalPlayer;
-
-// "use client";
-// // app/components/GlobalPlayer.tsx
-// import ReactPlayer from "react-player";
-// import { useSoundsStore } from "~/store/useSoundsStore";
-
-// interface GlobalPlayerProps {
-//   type?: string | string[]; // Accept a single string or an array of strings
-// }
-
-// const GlobalPlayer: React.FC<GlobalPlayerProps> = ({ type }) => {
-//   const { sounds } = useSoundsStore();
-//   const soundKeys = Object.keys(sounds);
-
-//   return (
-//     <>
-//       {soundKeys
-//         .filter((key) => {
-//           const sound = sounds[key];
-
-//           // Filter by type if a type is provided
-//           if (!sound) return false;
-//           if (!type) return true; // If no type is provided, include all
-
-//           const soundTypes = Array.isArray(type) ? type : [type];
-//           return soundTypes.includes(sound.soundType);
-//         })
-//         .map((key) => {
-//           const sound = sounds[key];
-
-//           // Check if sound exists before rendering the ReactPlayer
-//           if (!sound) return null;
-
-//           return (
-//             <ReactPlayer
-//               key={key}
-//               url={sound.url}
-//               playing={sound.playing}
-//               volume={sound.volume}
-//               controls={false} // Don't show controls as it's controlled globally
-//               width="0"
-//               height="0"
-//             />
-//           );
-//         })}
-//     </>
-//   );
-// };
-
-// export default GlobalPlayer;
