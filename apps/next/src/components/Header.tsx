2ca12a00f63518c84f081bac47554406652fa669
artifactz1 <punsalang.art@gmail.com>
2025-02-24 16:21:38 -0800

    added(other-components)

---
+++ apps/next/src/components/Header.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import SessionMobileCount from "./SessionMobileCount";
+import SessionTitleDisplay from "./SessionTitleDisplay";
+import SessionsUI from "./SessionsUI";
+
+function Timer() {
+  return (
+    <div className="flex w-screen flex-col-reverse px-10 pt-10 sm:mt-0 sm:h-[15vh] sm:w-full sm:flex-row sm:items-center sm:justify-between sm:px-[5vw]">
+      <SessionsUI />
+      <div className="mb-4 flex items-end justify-between sm:mb-1">
+        <SessionTitleDisplay />
+        <SessionMobileCount />
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
