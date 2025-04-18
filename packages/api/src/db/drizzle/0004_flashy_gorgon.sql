a567eee752cad0bb951ed300c026a802c5827b25
artifactz1 <punsalang.art@gmail.com>
2025-04-17 21:30:18 -0700

    still figuring out how to save session settings data /updating accounts button

---
+++ packages/api/src/db/drizzle/0004_flashy_gorgon.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "sessionSettings" ALTER COLUMN "work_duration" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "sessionSettings" ALTER COLUMN "break_duration" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "sessionSettings" ALTER COLUMN "number_of_sessions" DROP DEFAULT;
