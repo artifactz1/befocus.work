8e1f6e774492e7a496e3b9c6ffb64c38caf044ca
artifactz1 <punsalang.art@gmail.com>
2025-04-30 21:12:49 -0700

    removed ipod music player / implemented simple bgMusic sounds

--- apps/next/src/components/sounds/BgMusicSoundsButton.tsx
+++ apps/next/src/components/sounds/BgMusicSoundsButton.tsx
@@ -1,70 +1,48 @@
-// 'use client'
-
-// import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@repo/ui/accordion'
-// import { Separator } from '@repo/ui/separator'
-// import { useSoundsStore } from '~/store/useSoundsStore'
-// import SoundButton from './SoundButton'
-
-// export default function BgMusicSoundsButton() {
-//   const { sounds } = useSoundsStore()
-
-//   return (
-//     <main>
-//       <Separator className='my-4 bg-white' />
-
-//       <Accordion type='single' collapsible>
-//         <AccordionItem className='border-0' value='item-1'>
-//           <AccordionTrigger className='text-md font-bold'>Music Sounds</AccordionTrigger>
-//           <AccordionContent>
-//             <div className='space-y-4'>
-//               {Object.keys(sounds)
-//                 .filter(soundId => sounds[soundId]?.soundType === 'bgMusic')
-//                 .map(soundId => (
-//                   <SoundButton key={soundId} soundId={soundId} />
-//                 ))}
-//             </div>
-//           </AccordionContent>
-//         </AccordionItem>
-//       </Accordion>
-//       <Separator className='my-4 bg-white' />
-
-//       {/* <h3 className="text-center font-semibold mb-3"> Music Sounds</h3>
-//       <div className="space-y-4">
-//         {Object.keys(sounds)
-//           .filter((soundId) => sounds[soundId]?.soundType === "bgMusic")
-//           .map((soundId) => (
-//             <SoundButton key={soundId} soundId={soundId} />
-//           ))}
-//       </div> */}
-//     </main>
-//   )
-// }
-
 'use client'
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@repo/ui/accordion'
 import { Separator } from '@repo/ui/separator'
-import IpodMusicPlayer from './IpodMusicPlayer'
+import { useSoundsStore } from '~/store/useSoundsStore'
+import SoundButton from './SoundButton'
 
 export default function BgMusicSoundsButton() {
+  const { sounds } = useSoundsStore()
+
   return (
     <main>
       <Separator className='my-4 bg-white' />
 
-      <Accordion type='single' collapsible>
+      {/* <Accordion type='single' collapsible>
         <AccordionItem className='border-0' value='item-1'>
-          <AccordionTrigger className='text-md font-bold'>
-            Music Sounds
-          </AccordionTrigger>
-
+          <AccordionTrigger className='text-md font-bold'>Music Sounds</AccordionTrigger>
           <AccordionContent>
-            {/* ← here’s your new iPod UI */}
-            <IpodMusicPlayer />
+            <div className='space-y-4'>
+              {Object.keys(sounds)
+                .filter(soundId => sounds[soundId]?.soundType === 'bgMusic')
+                .map(soundId => (
+                  <SoundButton key={soundId} soundId={soundId} />
+                ))}
+            </div>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
+      <Separator className='my-4 bg-white' /> */}
 
-      <Separator className='my-4 bg-white' />
+      {/* <h3 className="text-center font-semibold mb-3"> Music Sounds</h3>
+      <div className="space-y-4">
+        {Object.keys(sounds)
+          .filter((soundId) => sounds[soundId]?.soundType === "bgMusic")
+          .map((soundId) => (
+            <SoundButton key={soundId} soundId={soundId} />
+          ))}
+      </div> */}
+      <div className='space-y-4'>
+        {Object.keys(sounds)
+          .filter(soundId => sounds[soundId]?.soundType === 'bgMusic')
+          .map(soundId => (
+            <SoundButton key={soundId} soundId={soundId} />
+          ))}
+      </div>
     </main>
   )
 }
+
