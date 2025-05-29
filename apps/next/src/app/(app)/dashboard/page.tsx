0a6a0e8969795481823c1870035f0fbe5590d9be
artifactz1 <punsalang.art@gmail.com>
2025-05-29 02:08:02 -0700

    fix/add(BgMusicSoundsButton/Loading Animation): fixed bug for BgMusic / create a loading animation

--- apps/next/src/app/(app)/dashboard/page.tsx
+++ apps/next/src/app/(app)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { motion } from 'framer-motion'
 import Footer from '~/components/Footer'
 import Header from '~/components/Header'
 import GlobalSoundsPlayer from '~/components/helper/GlobalSoundsPlayer'
@@ -12,11 +13,56 @@ export default function Dashboard() {
       <GlobalSoundsPlayer />
       <TimerInitializer />
       <PrefetchUserTasks />
-      <main className='continer px-auto flex h-screen w-screen flex-col items-center justify-center'>
+      <motion.main className='continer px-auto flex h-screen w-screen flex-col items-center justify-center'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, ease: 'easeOut', delay: 0.25 }}
+      >
         <Header />
         <Timer />
         <Footer />
-      </main>
+      </motion.main>
     </div>
   )
 }
+
+// 'use client'
+
+// import { motion } from 'framer-motion'
+// import { useEffect, useState } from 'react'
+// import Footer from '~/components/Footer'
+// import Header from '~/components/Header'
+// import LoadingAnimation from '~/components/LoadingAnimation'
+// import GlobalSoundsPlayer from '~/components/helper/GlobalSoundsPlayer'
+// import PrefetchUserTasks from '~/components/helper/PrefetchUserTasks'
+// import Timer from '~/components/timer/Timer'
+// import { TimerInitializer } from '~/components/timer/TimerInitializer'
+
+// export default function Dashboard() {
+//   const [isLoading, setIsLoading] = useState(true)
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => setIsLoading(false), 1000)
+//     return () => clearTimeout(timer)
+//   }, [])
+
+//   return (
+//     <>
+
+//       <GlobalSoundsPlayer />
+//       <TimerInitializer />
+//       <PrefetchUserTasks />
+
+//       <motion.main
+//         className="container px-auto flex h-screen w-screen flex-col items-center justify-center"
+//         initial={{ opacity: 0 }}
+//         animate={{ opacity: isLoading ? 0 : 1 }}
+//         transition={{ duration: 0.6, ease: 'easeOut' }}
+//       >
+//         <Header />
+//         <Timer />
+//         <Footer />
+//       </motion.main>
+//     </>
+//   )
+// }
