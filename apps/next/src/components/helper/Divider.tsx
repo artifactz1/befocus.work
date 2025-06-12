898742d4f8642d3547d057c3d576a395dcc482e3
artifactz1 <punsalang.art@gmail.com>
2025-06-11 22:38:36 -0700

    update(divider) changed colors and thickness of line

--- apps/next/src/components/helper/Divider.tsx
+++ apps/next/src/components/helper/Divider.tsx
@@ -1,11 +1,10 @@
 import { Separator } from '@repo/ui/separator'
-import React from 'react'
 import clsx from 'clsx'
 
-type DividerProps = { 
+type DividerProps = {
   className?: string
 }
 
-export default function Divider({className} : DividerProps) {
-  return <Separator className={clsx('my-4 bg-black dark:bg-white', className)}/>
+export default function Divider({ className }: DividerProps) {
+  return <Separator className={clsx('my-4 bg-stone-400 h-0.5 dark:bg-stone-700', className)} />
 }
